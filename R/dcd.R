4138a42c707f2afc6ad2154eccd7493ee81b192e
alchiba <alain.corvisier@proton.me>
2025-01-28 22:43:49 +0100

    modify db_get_nom and db_get_prenoms functions
    - To gain speed
    - Changed from stri_split_fixed to stri_match_all_regex

--- R/dcd.R
+++ R/dcd.R
@@ -9,75 +9,100 @@ library(purrr)
 library(stringi)
 
 dossiers <- c(
-  donnees_raw   = 'inst/rawdata',
-  donnees_deces = file.path(donnees_raw, 'deces'),
-  donnees       = 'data'
+  donnees_raw   <- 'inst/rawdata',
+  donnees_deces <- file.path(donnees_raw, 'deces'),
+  donnees       <- 'data'
 )
 
 # Création des dossiers
-sapply(dossiers, \(x) { if (!dir.exists(x)) dir.create(x, recursive = TRUE) })
+invisible(
+  sapply(dossiers, \(x) { if (!dir.exists(x)) dir.create(x, recursive = TRUE) })
+)
+
 
 # Décodage des données
 taille_donnees <- c(
-  nom                  = 80,
-  sexe                 =  1,
-  naissance_date       =  8,
-  naissance_code_insee =  5,
-  naissance_commune    = 30,
-  naissance_pays       = 30,
-  deces_date           =  8,
-  deces_code_insee     =  5,
-  deces_numero_acte    =  9
+  nom_prenoms           = 80,
+  sexe                  =  1,
+  naissance_date_string =  8,
+  naissance_code_insee  =  5,
+  naissance_commune     = 30,
+  naissance_pays        = 30,
+  deces_date_string     =  8,
+  deces_code_insee      =  5,
+  deces_numero_acte     =  9
 )
 
-chemins_fichiers_deces <- list.files(donnees_deces,
+paths_fichiers_deces <- list.files(donnees_deces,
                                      pattern = "[0-9]{4}[.]txt$",
+                                     # pattern = "197[0-9][.]txt$",
                                      full.names = TRUE)
 
-dbs_raw_deces <- chemins_fichiers_deces |> 
+db_raw_deces <- paths_fichiers_deces |> 
   map(read_fwf, 
       col_positions = fwf_widths(taille_donnees, 
                                  col_names = names(taille_donnees)),
       col_types = cols(
         .default = col_character())
-  )
-
-db <- bind_rows(dbs_raw_deces) |> 
-  unique()
-
-db_get_nom_prenoms <- function(string) {
+  ) 
   
-  string <- gsub(
-    pattern = "/", # symbole parasite
-    replacement = "",
-    x = string
-  )
-  nom_prenoms <- stri_split_fixed(
-    string, 
-    pattern = "*"
-    )
-  nom <- nom_prenoms[[1]][1]
-  prenoms <- stri_split_fixed(
-    nom_prenoms[[1]][2],
-    pattern = " "
-  ) |> 
-    unlist()
-  
-  return(list(nom = nom, prenoms = prenoms))
-  
-} 
-
+#
+#-------------------------------------------------------------------------------
+# Format nom_prenoms : 
+# "<nom>*<prenom1> <prenom2> <prenom3>...<last_prenom>/" 
+# 
 db_get_nom <- function(string) {
   
-  return(db_get_nom_prenoms(string)$nom)
-
+  pattern_nom = "(.*?)[*]"
+  nom <- stri_match_all_regex(
+    str = string,
+    pattern = pattern_nom
+    )[[1]][,2]
+  
+  return(nom)
+  
 }
 
 db_get_prenoms <- function(string) {
   
-  return(db_get_nom_prenoms(string)[[2]])
+  pattern_nom = "[*](.*)[/]$"
+  prenoms <- stri_match_all_regex(
+    str = string,
+    pattern = pattern_nom
+  )[[1]][,2]
+  
+  return(prenoms)
   
 }
 
-db_get_nom(db$nom[1:10])
+db_get_date <- function(string, format = "%Y%m%d") {
+  
+    # return a date or NA if date is not VALID
+    date_event = as.Date(string, format = format, optional = TRUE)
+  
+  return(date_event)
+  
+}
+#
+#-------------------------------------------------------------------------------
+#
+
+db_deces <- db_raw_deces |> 
+  bind_rows() |> 
+  unique() |> 
+  mutate( # conversion en UTF-8 à cause de "CS\xa8ND\xa8R*LASZLO/"
+    nom_prenoms = iconv(nom_prenoms, from = "ISO-8859-1", to = "UTF-8")
+  )
+
+rm(db_raw_deces)
+
+db_deces <- db_deces |>   
+  rowwise() |> 
+  mutate(
+    nom = db_get_nom(nom_prenoms),
+    prenoms = db_get_prenoms(nom_prenoms),
+    naissance_date = db_get_date(naissance_date_string),
+    deces_date = db_get_date(deces_date_string),
+  ) 
+
 
