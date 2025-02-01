edf2093a76b70ddb7d3dfff72325b1fe6ca17505
alchiba <alain.corvisier@proton.me>
2025-02-01 23:43:09 +0100

    append data to postgresql remote database

---
+++ R/02_sql_section.R
@@ -0,0 +1,9 @@
+# append data from db_deces_4_sql.RDS
+
+db_deces_4_sql <- readRDS(file = file.path(dossier_data, "db_deces_4_sql.RDS"))
+
+RPostgres::dbAppendTable(
+  con = con, 
+  name = Id(schema = 'dcd', table = 'deces'), 
+  value = db_deces_4_sql
+)
